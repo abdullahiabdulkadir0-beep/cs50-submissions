57ced0e93d785e3d0c536028458eaf118b9171e0
abdullahiabdulkadir51-byte <abdullahiabdulkadir51-byte@users.noreply.github.com>
2025-10-14 16:48:13 +0100

    automated commit by submit50

--- plurality.c
+++ plurality.c
@@ -10,8 +10,7 @@ typedef struct
 {
     string name;
     int votes;
-}
-candidate;
+} candidate;
 
 // Array of candidates
 candidate candidates[MAX];
@@ -66,41 +65,43 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    //Loop through the Candidate count
+    // Loop through the Candidate count
     for (int i = 0; i < candidate_count; i++)
     {
-        //check if candidate is similar to what is user's vote
+        // check if candidate is similar to what is user's vote
         if (strcmp(candidates[i].name, name) == 0)
         {
-            //if similar to what user vote for, then increase the specific candidate vote and return true
+            // if similar to what user vote for, then increase the specific candidate vote and
+            // return true
             candidates[i].votes++;
             return true;
         }
     }
-    //else if not similar to candidate vote return false.....and print invalid vote
+    // else if not similar to candidate vote return false.....and print invalid vote
     return false;
 }
 
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    //Create and variable and set it to 0
+    // Create and variable and set it to 0
     int maximum_vote = 0;
 
-    //iterate over list of candidate
+    // iterate over list of candidate
     for (int i = 0; i < candidate_count; i++)
     {
-        //check for candidate votes that are  greater than maximum_vote and set them to maximum_vote
+        // check for candidate votes that are  greater than maximum_vote and set them to
+        // maximum_vote
         if (candidates[i].votes > maximum_vote)
         {
             maximum_vote = candidates[i].votes;
         }
     }
 
-    //iterate over list of candidate
+    // iterate over list of candidate
     for (int i = 0; i < candidate_count; i++)
     {
-        //check for candidate votes that are equal to maximum vote and print them as you go
+        // check for candidate votes that are equal to maximum vote and print them as you go
         if (candidates[i].votes == maximum_vote)
         {
             printf("%s\n", candidates[i].name);
